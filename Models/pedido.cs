f60ab6cb9c2da0f46967bab13efc84a688257728
ThonyFM <137853537+ThonyFM@users.noreply.github.com>
2025-02-24 20:12:50 -0600

    Modelo listo

--- Models/pedido.cs
+++ Models/pedido.cs
@@ -1,7 +1,50 @@
-﻿namespace WebAppForm.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppForm.Models
 {
     public class pedido
     {
+        [Key]
+        public int productoId { get; set; }
+
+        [Required, MaxLength(150)]
+        public string nombre { get; set; }
+
+        [Required, MaxLength(150)]
+        public string descripcion { get; set; }
+
+        [Required]
+        public decimal precio { get; set; }
+
+        [Required, MaxLength(50)]
+        public string AdicionadoPor { get; set; }
+
+        [Required, DataType(DataType.DateTime)]
+        public DateTime FechaAdicion { get; set; } = DateTime.Now;
+
+        [MaxLength(50)]
+        public string ModificadoPor { get; set; }
+        
+        [DataType(DataType.DateTime)]
+        public DateTime? FechaModificacion { get; set; }
+
+        public pedido()
+        {
+            
+        }
+   
+        public pedido(int productoId, string nombre, string descripcion, decimal precio, string adicionadoPor, string modificadoPor, DateTime? fechaModificacion)
+        {
+            this.productoId = productoId;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            AdicionadoPor = adicionadoPor;
+            ModificadoPor = modificadoPor;
+            FechaModificacion = fechaModificacion;  
+        }
 
     }
 }
+
--- Models/producto.cs
+++ Models/producto.cs
@@ -1,7 +1,51 @@
-﻿namespace WebAppForm.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppForm.Models
 {
     public class producto
     {
+        [Key] public int pedidoId { get; set; }
 
+        [Required, DataType(DataType.DateTime)]
+        public DateTime fecha { get; set; } = DateTime.Now;
+
+        [Required]
+        public decimal total { get; set; }
+
+        [Required, MaxLength(50)] public string estado { get; set; }
+
+        [Required] public int clienteId { get; set; }
+        [Required] public int productoId { get; set; }
+
+        public string AdicionadoPor { get; set; }
+
+        [Required, DataType(DataType.DateTime)]
+        public DateTime FechaAdicion { get; set; } = DateTime.Now;
+
+        [MaxLength(50)]
+        public string ModificadoPor { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? FechaModificacion { get; set; }
+
+        public producto()
+        {
+            
+        }
+
+        public producto(int pedidoId, decimal total, string estado, int clienteId, int productoId, string adicionadoPor, string modificadoPor, DateTime? fechaModificacion)
+        {
+            this.pedidoId = pedidoId;
+            this.total = total;
+            this.estado = estado;
+            this.clienteId = clienteId;
+            this.productoId = productoId;
+            AdicionadoPor = adicionadoPor;
+            ModificadoPor = modificadoPor;
+            FechaModificacion = fechaModificacion;  
+        }
     }
+
+
 }
