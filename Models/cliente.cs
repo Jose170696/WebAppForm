86d0d169733c9e05310aaf935974c74880c0c100
jose170696 <esteban_chz@hotmail.com>
2025-02-24 19:57:59 -0600

    Add project files.

---
+++ Models/cliente.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppForm.Models
+{
+    public class cliente
+    {
+        public int clienteId { get; set; }
+        [Required, MaxLength(150)]
+        public string nombre { get; set; }
+        [Required, EmailAddress, MaxLength(150)]
+        public string email { get; set; }
+        [Required, Phone, MaxLength(150)]
+        public string telefono { get; set; }
+        [Required, MaxLength(200)]
+        public string direccion { get; set; }
+        [Required, MaxLength(50)]
+        public string AdicionadoPor { get; set; }
+        [Required, DataType(DataType.DateTime)]
+        public DateTime FechaAdicion { get; set; } = DateTime.Now;
+        [MaxLength(50)]
+        public string ModificadoPor { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime? FechaModificacion { get; set; }
+
+        public cliente()
+        {
+
+        }
+
+
+    }
+}
